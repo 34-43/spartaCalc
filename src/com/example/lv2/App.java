842e8bb2e7732207b4d40f41adb6af5fecddb6b6
34-43 <121276581+34-43@users.noreply.github.com>
2024-09-04 17:57:02 +0900

    feat: lv2 App, Calculator 클래스 생성

---
+++ src/com/example/lv2/App.java
@@ -0,0 +1,7 @@
+package com.example.lv2;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/example/lv2/Calculator.java
@@ -0,0 +1,34 @@
+package com.example.lv2;
+
+import java.util.Queue;
+
+public class Calculator {
+    private int operandLeft, operandRight;
+    private char operator;
+    private Queue<Integer> resultQueue;
+
+    public Calculator() {
+
+    }
+
+    public int setLeft(int n) {
+        return 0;
+    }
+    public int setRight(int n) {
+        return 0;
+    }
+    public int setOperator(char ch) {
+        return 0;
+    }
+
+    public int getResult() {
+        return 0;
+    }
+    public int getResult(int at) {
+        return 0;
+    }
+    public void removeResult() {
+
+    }
+
+}
