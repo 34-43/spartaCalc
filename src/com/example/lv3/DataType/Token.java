884c1e729246af46c8bf45239123ca5463d2c9dc
34-43 <121276581+34-43@users.noreply.github.com>
2024-09-10 23:20:31 +0900

    feat: lv3 Token 타입 및 Tokenizer 유틸 클래스 구현

---
+++ src/com/example/lv3/DataType/Token.java
@@ -0,0 +1,41 @@
+package com.example.lv3.DataType;
+
+import com.example.lv3.ArithmeticOperator;
+
+public class Token<T> {
+    public enum TokenType {
+        INTEGER, FLOAT, ARITHMETIC, PARENTHESES_LEFT, PARENTHESES_RIGHT,
+    }
+    private TokenType type;
+    private T value;
+
+    public Token(TokenType type, T value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public static int getPriority(Token<ArithmeticOperator.OperatorType> mightBigger, Token<ArithmeticOperator.OperatorType> mightSmaller) {
+        return mightBigger.getValue().compareTo(mightSmaller.getValue());
+    }
+
+    @Override
+    public String toString() {
+        return "[타입:" + type + "," + "값:" + value + "]";
+    }
+
+    public static void main(String[] args) {
+        Token<ArithmeticOperator.OperatorType> A = new Token<>(TokenType.ARITHMETIC, ArithmeticOperator.OperatorType.ADD);
+        Token<ArithmeticOperator.OperatorType> B = new Token<>(TokenType.ARITHMETIC, ArithmeticOperator.OperatorType.MULTIPLY);
+        System.out.println(getPriority(A,A));
+        System.out.println(getPriority(A,B));
+        System.out.println(getPriority(B,A));
+    }
+}
---
+++ src/com/example/lv3/Tokenizer.java
@@ -0,0 +1,55 @@
+package com.example.lv3;
+
+import com.example.lv3.DataType.Token;
+import com.example.lv3.Exceptions.TokenizerException;
+import com.example.lv3.DataType.Token.TokenType;
+import com.example.lv3.ArithmeticOperator.OperatorType;
+
+// 정규표현과 제어문을 통해 입력된 문자열 토큰을 적절한 토큰 객체로 반환해 주는 유틸 클래스.
+// 입력되는 문자열 토큰은 정확해야 하며, 잘못되었을 경우 사용자 지정 예외를 던집니다.
+
+public class Tokenizer {
+    private static final String INTEGER_REG ="^[0-9]*$";
+    private static final String FLOAT_REG ="^[0-9]*\\.?[0-9]+$";
+    private static final String OPERATION_REG = "[+\\-*/]";
+    
+    public static Token<?> parseToken(String input) throws TokenizerException {
+        if (input.matches(INTEGER_REG)) {
+            return new Token<>(TokenType.INTEGER,Integer.parseInt(input));
+        } else if (input.matches(FLOAT_REG)) {
+            return new Token<>(TokenType.FLOAT,Float.parseFloat(input));
+        } else if (input.matches(OPERATION_REG)) {
+            OperatorType ot = switch (input) {
+                case "+" -> OperatorType.ADD;
+                case "-" -> OperatorType.SUBTRACT;
+                case "*" -> OperatorType.MULTIPLY;
+                case "/" -> OperatorType.DIVIDE;
+                default -> throw new TokenizerException("연산자:" + input);
+            };
+            return new Token<OperatorType>(TokenType.ARITHMETIC,ot);
+        } else if (input.equals("(")) {
+            return new Token<TokenType>(TokenType.PARENTHESES_LEFT,TokenType.PARENTHESES_LEFT);
+        } else if (input.equals(")")) {
+            return new Token<TokenType>(TokenType.PARENTHESES_RIGHT,TokenType.PARENTHESES_RIGHT);
+        } else {
+            throw new TokenizerException(input);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] testCases = {
+                "1",
+                "5.5",
+                "+",
+                "(",
+                "g"
+        };
+        for (String testCase : testCases) {
+            try {
+                System.out.println(parseToken(testCase));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
