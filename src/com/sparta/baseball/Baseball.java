b8c2ecf45fbc89c850b68a3f8d947db023272927
34-43 <121276581+34-43@users.noreply.github.com>
2024-09-20 16:09:31 +0900

    feat: Baseball 추측 횟수 관련 기능 2가지 추가 추측 횟수 필드 및 메서드 추가(guessCount, getGuessCount, increaseGuessCount, saveResult) 통계 용 정적 필드 및 메서드 추가(statList, getStatArray)

--- src/com/sparta/baseball/Baseball.java
+++ src/com/sparta/baseball/Baseball.java
@@ -11,19 +11,21 @@ Baseball 객체는 정수 배열과 Integer Set 의 두 필드를 이용하여 �
  */
 
 public class Baseball {
-    // 정답 수열을 저장하는 정수 배열과 Integer Set 입니다. 이 중 Set 은 ball 계산에 지속적으로 사용됩니다.
-    private int[] answerArr;
-    private final Set<Integer> answerSet;
+    private int[] answerArr;    // 정답 수열을 저장하는 정수 배열.
+    private final Set<Integer> answerSet;   // 정답 수열의 생성 및 추측 계산에 필요한 Integer Set 콜렉션.
+    private int guessCount; // 추측 시도 횟수를 가산하는 정수 변수.
+    private static final ArrayList<Integer> statList = new ArrayList<>();   // 프로그램이 종료되기 전까지 통계를 저장할 정적 리스트 입니다.
 
     // 필드를 초기화하는 유일한 기본 생성자입니다.
     public Baseball() {
         this.answerArr = new int[3];
         answerSet = new HashSet<>();
+        guessCount = 0;
     }
 
     // 새로운 게임을 시작하기 위해, 정답을 생성하는 메서드입니다.
     // 1~9의 난수를 생성하여 필드 Set 의 크기가 정해진 크기가 되도록 하고, 이것을 배열로 변환하여 순서성이 부여되도록 합니다.
-    // 변환 결과가 오름차순이 되는 것을 막기 위해 결과를 한 번 뒤섞습니다.
+    // 변환 결과가 오름차순이 되는 것을 막기 위해 결과를 한 번 뒤섞습니다. 시도 횟수 또한 초기화합니다.
     public void newGame() {
         answerSet.clear();
         while(answerSet.size() < 3) {
@@ -33,11 +35,22 @@ public class Baseball {
         List<Integer> ls = new ArrayList<>(answerSet);
         Collections.shuffle(ls);
         answerArr = ls.stream().mapToInt(i -> i).toArray();
+        guessCount = 0;
     }
 
     // 정답 배열을 연이은 3자리 숫자 형태의 문자열로 변환하여 반환합니다.
     public String getAnswer() {
         return Integer.toString(Arrays.stream(answerArr).reduce((a,b)->10*a+b).orElse(0));
+    }
+
+    // guessCount 의 getter 입니다.
+    public int getGuessCount() {
+        return guessCount;
+    }
+
+    // 추측 시도 횟수를 가산하는 private 메서드입니다.
+    private void increaseGuessCount() {
+        guessCount++;
     }
 
     // 전달된 입력값을 검사하고, 정답 필드와 비교하여 그 결과를 GuessResult 객체로 반환합니다.
@@ -50,7 +63,21 @@ public class Baseball {
             if (answerArr[i] == input.charAt(i) - '0') {strike++;}
             if (answerSet.contains(input.charAt(i) - '0')) {ball++;}
         }
+        increaseGuessCount();
         return new GuessResult(strike, ball - strike);
+    }
+
+    // 추측이 성공적인 경우 호출되어, 현재 추측 시도 횟수를 통계에 저장합니다.
+    public void saveResult() {
+        if (guessCount > 0) {
+            statList.add(guessCount);
+            guessCount = 0;
+        }
+    }
+
+    // 통계를 int Array 형태로 반환하는 getter 입니다.
+    public static int[] getStatArray() {
+        return statList.stream().mapToInt(i -> i).toArray();
     }
 
     // 콘솔에 아스키 아트 야구선수를 출력해 주는 정적 메서드입니다.
