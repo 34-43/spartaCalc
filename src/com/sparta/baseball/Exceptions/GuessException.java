e0c5c77be2b2651f587e01b934ff6f5a218a96fa
34-43 <121276581+34-43@users.noreply.github.com>
2024-09-23 20:33:48 +0900

    chore: GuessException 주석 교정

--- src/com/sparta/baseball/Exceptions/GuessException.java
+++ src/com/sparta/baseball/Exceptions/GuessException.java
@@ -1,7 +1,7 @@
 package com.sparta.baseball.Exceptions;
 
 /*
-추측 시도가 조건을 만족하지 못했을 경우, 해당 조건을 들어 부적절함을 고지하는 예외 처리 클래스입니다.
+추측 시도가 조건을 만족하지 못했을 경우, 해당 조건을 들어 부적절함을 고지하는 예외 클래스입니다.
  */
 
 public class GuessException extends Exception {
